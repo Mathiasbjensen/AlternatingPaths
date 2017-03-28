9f230c93af24e13d4a8c7fb7f118a91a4d3dbe78
Mathias Jensen <Mathiasbj94@gmail.com>
2017-03-28 11:37:11 +0200

    Added svans

--- AlternatingPaths/src/AlgoExam09.java
+++ AlternatingPaths/src/AlgoExam09.java
@@ -5,6 +5,7 @@ public class AlgoExam09 {
 
 	public static void main(String[] args) {
 		
+		//Svans
 		AlgoExam09 rekursiv = new AlgoExam09();
         Scanner scanner = new Scanner(System.in);
         
