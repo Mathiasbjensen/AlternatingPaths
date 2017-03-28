31ade1a8aac0733427fb15acd427229eb09e44c0
Mathias Jensen <mathiasbj94@gmail.com>
2017-03-28 11:39:33 +0200

    Added Skod

--- AlternatingPaths/src/AlgoExam09.java
+++ AlternatingPaths/src/AlgoExam09.java
@@ -6,6 +6,7 @@ public class AlgoExam09 {
 	public static void main(String[] args) {
 		
 		//Svans
+		//Skod
 		AlgoExam09 rekursiv = new AlgoExam09();
         Scanner scanner = new Scanner(System.in);
         
